806e3b3b4259ee9dee81947cc9581cc918f1aafb
Christopher Teng <c.s.teng@wustl.edu>
2023-12-15 22:04:42 -0600

    final changes

--- src/pages/services.js
+++ src/pages/services.js
@@ -7,34 +7,57 @@ import { useEffect } from "react"
 const Services = ({location}) => {
     const provider = location.state?.provider || '';
     const service = location.state?.service || '';
+    console.log(provider);
+    console.log(service);
 
     const filteredData = compiledData.filter(item => 
-        item.ServiceType === service && 
-        item.Provider.toLowerCase().includes(provider.toLowerCase())
+        (item.ServiceType === service) && 
+        (item.Provider.toLowerCase().includes(provider.toLowerCase()) || item.Provider === "All")
     );
     console.log(filteredData);
 
+    const serviceContainerStyle = {
+        position: 'absolute',
+        top: '15%', 
+        left: '5%',
+        width: '100%',
+        height: 'auto', 
+        overflow: 'auto',
+    };
     // CSS style for horizontal scrolling
     const horizontalScrollStyle = {
-        display: 'flex',
-        flexDirection: 'row',
-        overflowX: 'auto',
-        paddingLeft: '500px', // Increased horizontal padding
-        gap: '10px',
-        alignItems: 'center', // Ensure cards are aligned centrally vertically
-        height: '100%',
-};
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)', // 4 columns with equal width
+        gap: '10px', // Space between cards
+        justifyContent: 'center', // Center the grid horizontally
+        alignItems: 'center', // Center the grid vertically
+        width: '100%', // Full width
+        height: '80%',
+        margin: 'auto' // Center the grid container
+    };
+
+    const noServicesStyle = {
+        textAlign: 'center', // Center the text
+        fontSize: '50px', // Relatively large font size
+        fontWeight: 'bold', // Bold text
+    };
 
     function renderTable(data) {
+        if (data.length === 0) {
+            return <div style={noServicesStyle}>No services found for this provider!</div>;
+        }
+
         return (
-            <div style={horizontalScrollStyle}>
-                {data.map((item, index) => (
-                    <ServiceCard 
-                    key={index} 
-                    service={item.ResourceName} 
-                    location={`${item.Street}, ${item.City}, ${item.State} ${item.ZipCode}`} 
-                    index={index} />
-                ))}
+            <div style={serviceContainerStyle}>
+                <div style={horizontalScrollStyle}>
+                    {data.map((item, index) => (
+                        <ServiceCard 
+                        key={index} 
+                        service={item.ResourceName} 
+                        location={`${item.Street}, ${item.City}, ${item.State} ${item.ZipCode}`} 
+                        index={index} />
+                    ))}
+                </div>
             </div>
         );
     }
